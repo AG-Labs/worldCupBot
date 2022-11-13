6cc7c434eff4a3e47dabbb5e9d840951e8b3b934
Andrew <agdevlabs@gmail.com>
2022-11-13 17:54:32 +0000

    Initial commit

---
+++ test/world_cup.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WorldCup from '../lib/world_cup-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/world_cup-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WorldCup.WorldCupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
