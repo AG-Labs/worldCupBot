dfda2f877cc72e304dfe55fc522ddaad8678ce8d
Andrew <agdevlabs@gmail.com>
2022-11-13 23:23:33 +0000

    attempt to make a rule for each game but struggling to stop cdk making a permission for each rule, change tak tomorrow and make a single catch all rule but add more to the lambda basic infra updated to run rule on mass a working start of game message messages at the end and middle now as well more edits and remove some secrets and move consts around All commits have been squashed together since there was a slack secret in the commits

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# Welcome to your CDK TypeScript project
+# Sweepstake bot
 
-This is a blank project for CDK development with TypeScript.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+The groups file can be manually triggered to see if someone has lost all their teams when moving from a group stage to knockouts.
+using the array of already out people you can run again at certain points in the knockout stage, maybe make this an output of running the script
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
--- lib/world_cup-stack.ts
+++ lib/world_cup-stack.ts
@@ -1,6 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { Rule, Schedule } from "aws-cdk-lib/aws-events";
+import * as eventTargets from "aws-cdk-lib/aws-events-targets";
+import {
+  Role,
+  ServicePrincipal,
+  CompositePrincipal,
+  PolicyStatement,
+  Effect,
+  ManagedPolicy,
+} from "aws-cdk-lib/aws-iam";
+import * as path from "path";
+import { Duration } from "aws-cdk-lib";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class WorldCupStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +23,72 @@ export class WorldCupStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'WorldCupQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new Queue(this, "WorldCupQueue", {});
+
+    const lambdaRole = new Role(this, "lambdaRole", {
+      assumedBy: new CompositePrincipal(
+        new ServicePrincipal("lambda.amazonaws.com"),
+        new ServicePrincipal("events.amazonaws.com")
+      ),
+      description: "Example role...",
+      managedPolicies: [
+        new ManagedPolicy(this, "mp1", {
+          managedPolicyName: "mp1",
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ["lambda:*"],
+              resources: ["*"],
+            }),
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ["logs:*"],
+              resources: ["*"],
+            }),
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ["sqs:*"],
+              resources: ["*"],
+            }),
+          ],
+        }),
+      ],
+    });
+    lambdaRole.addToPolicy;
+
+    const fn = new Function(this, "function", {
+      runtime: Runtime.NODEJS_16_X,
+      handler: "handler.index",
+      code: Code.fromAsset(path.join(__dirname, "Lambda")),
+      role: lambdaRole,
+      environment: {
+        sqsQueue: queue.queueUrl,
+        apiKey: "",
+        slackWebhookUrl: "",
+      },
+      timeout: Duration.seconds(20),
+    });
+
+    fn.addPermission("ebp", {
+      principal: new ServicePrincipal("events.amazonaws.com"),
+      sourceArn: `arn:aws:events:${this.region}:${this.account}:rule/*`,
+    });
+    fn.addEventSource(new SqsEventSource(queue));
+
+    const rule = new Rule(this, `timed-events-game`, {
+      description: "Timed Rule for world cup games",
+      enabled: true,
+      ruleName: `game-cron`,
+      schedule: Schedule.cron({
+        minute: "0",
+        hour: "16,13,19,10,15",
+        day: "20,21,22,23,24,25,26,27,28,29,30,01,02,03,04,05,06,09,10,13,14,17,18",
+        month: "11,12",
+      }),
+      targets: [new eventTargets.LambdaFunction(fn)],
+    });
+
+    // eventTargets.addLambdaPermission(rule, fn);
+    // rule.addTarget(new eventTargets.LambdaFunction(fn));
   }
 }
